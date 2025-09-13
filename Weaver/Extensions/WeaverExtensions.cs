a8fb5f915acfecf6761bf533c067f40284c66369
Pallab Banerjee <mailme@pallabbanerjee.in>
2025-09-13 19:38:48 +0530

    chore : Added Extension method for nullable value type and added MapSingleFromReaderAsync method, enhanced the MapperGenerator.cs to support strict mapping

--- Weaver/Extensions/WeaverExtensions.cs
+++ Weaver/Extensions/WeaverExtensions.cs
@@ -4,17 +4,64 @@ using Weaver.Abstractions;
 
 namespace Weaver.Extensions;
 
+/// <summary>
+/// Provides extension methods for registering Weaver database services 
+/// into an <see cref="IServiceCollection"/>.
+/// </summary>
+/// <remarks>
+/// These extensions simplify dependency injection (DI) setup by adding the 
+/// required database components (e.g., <see cref="IDbConnectionFactory"/> 
+/// and <see cref="IDbClient"/>) into the service container.
+/// </remarks>
 public static class WeaverExtensions
 {
+    /// <summary>
+    /// Registers an <see cref="IDbConnectionFactory"/> implementation in the 
+    /// dependency injection container using the specified <see cref="DbProviderFactory"/> 
+    /// and connection string.
+    /// </summary>
+    /// <param name="services">
+    /// The <see cref="IServiceCollection"/> to add the service registration to.
+    /// </param>
+    /// <param name="dbProviderFactory">
+    /// The <see cref="DbProviderFactory"/> used to create database connections.
+    /// </param>
+    /// <param name="connectionString">
+    /// The database connection string to be used by the connection factory.
+    /// </param>
+    /// <returns>
+    /// The same <see cref="IServiceCollection"/> instance so that additional calls can be chained.
+    /// </returns>
+    /// <remarks>
+    /// This method registers <see cref="IDbConnectionFactory"/> as a singleton, ensuring that 
+    /// a single factory instance is reused throughout the application's lifetime.
+    /// </remarks>
     public static IServiceCollection AddConnectionFactory(this IServiceCollection services, DbProviderFactory dbProviderFactory, string connectionString)
     {
-        services.AddSingleton<IDbConnectionFactory>(sp =>
-        {
-            return new DbConnectionFactory(dbProviderFactory, connectionString);
-        });
-
+        services.AddSingleton<IDbConnectionFactory>(sp => new DbConnectionFactory(dbProviderFactory, connectionString));
         return services;
     }
+    /// <summary>
+    /// Registers the <see cref="WeaverSqlClient"/> as the implementation for <see cref="IDbClient"/> 
+    /// in the dependency injection container.
+    /// </summary>
+    /// <param name="services">
+    /// The <see cref="IServiceCollection"/> to add the service registration to.
+    /// </param>
+    /// <returns>
+    /// The same <see cref="IServiceCollection"/> instance so that additional calls can be chained.
+    /// </returns>
+    /// <remarks>
+    /// <para>
+    /// This method registers <see cref="IDbClient"/> as a singleton. It resolves an 
+    /// <see cref="IDbConnectionFactory"/> from the service provider and uses it to create 
+    /// a <see cref="WeaverSqlClient"/> instance.
+    /// </para>
+    /// <para>
+    /// Make sure <see cref="IDbConnectionFactory"/> has been registered in the container 
+    /// (for example, by calling <c>AddConnectionFactory</c>) before using this method.
+    /// </para>
+    /// </remarks>
     public static IServiceCollection AddWeaverSqlClient(this IServiceCollection services)
     {
         services.AddSingleton<IDbClient>(sp =>
--- Weaver/WeaverSqlClient.cs
+++ Weaver/WeaverSqlClient.cs
@@ -3,14 +3,40 @@ using Weaver.Abstractions;
 
 namespace Weaver;
 
+/// <summary>
+/// A concrete implementation of <see cref="IDbClient"/> that provides database access using a connection factory.
+/// </summary>
+/// <remarks>
+/// <para>
+/// This class is responsible for executing SQL queries, mapping results to objects, 
+/// and managing connections obtained from an <see cref="IDbConnectionFactory"/>.
+/// </para>
+/// <para>
+/// It supports asynchronous operations, parameterized queries, and proper cancellation via <see cref="CancellationToken"/>.
+/// </para>
+/// <para>
+/// Typically used in repository or service classes to interact with the database in a safe and reusable manner.
+/// </para>
+/// </remarks>
 public sealed class WeaverSqlClient : IDbClient
 {
     private readonly IDbConnectionFactory _dbConnectionFactory;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WeaverSqlClient"/> class.
+    /// </summary>
+    /// <param name="dbConnectionFactory">
+    /// The <see cref="IDbConnectionFactory"/> used to create database connections.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="dbConnectionFactory"/> is <c>null</c>.
+    /// </exception>
     public WeaverSqlClient(IDbConnectionFactory dbConnectionFactory)
     {
-        _dbConnectionFactory = dbConnectionFactory;
+        _dbConnectionFactory = dbConnectionFactory ?? throw new ArgumentNullException(nameof(dbConnectionFactory));
     }
+    
+    ///<inheritdoc/>
     public Task<IReadOnlyList<T>> QueryAsync<T>(string sqlQuery, IEnumerable<KeyValuePair<string, object>> parameters, CommandType commandType = CommandType.Text, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
